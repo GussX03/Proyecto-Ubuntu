88865c5bfc96cc61de31512697ec69bdea567ac9
GussUbuntu <gustavotlx03@gmail.com>
2024-04-24 15:26:44 -0600

    SUBIDA DE PROYECTO

---
+++ interfaz.c
@@ -0,0 +1,301 @@
+#include <gtk/gtk.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define SIZE 255
+#define COLA_MENSAJES 1234
+int mi_socket;
+    int tam, numbytes;
+    char buffer[SIZE];
+    struct sockaddr_in mi_estructura;
+    struct solicitud nueva_solicitud;
+
+
+struct solicitud {
+    long tipo;
+    char nombre[50];
+    char destino[50];
+    char horario[50];
+    char boleto_tipo[50];
+};
+
+
+void on_button_clicked(GtkWidget *widget, gpointer data) {
+    const gchar *nombre = gtk_entry_get_text(GTK_ENTRY(data));
+    const gchar *destino = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(data + 1));
+    const gchar *horario_txt;
+    const gchar *boleto_tipo;
+    gdouble horario = gtk_spin_button_get_value(GTK_SPIN_BUTTON(data + 3));
+
+    switch (gtk_combo_box_get_active(GTK_COMBO_BOX(data + 2))) {
+        case 0:
+            horario_txt = "Matutino 7-8 AM";
+            break;
+        case 1:
+            horario_txt = "Matutino 9-10 AM";
+            break;
+        case 2:
+            horario_txt = "Matutino 11-12 AM";
+            break;
+        case 3:
+            horario_txt = "Vespertino 1-3 PM";
+            break;
+        case 4:
+            horario_txt = "Vespertino 3-5 PM";
+            break;
+        case 5:
+            horario_txt = "Vespertino 5-7 PM";
+            break;
+        case 6:
+            horario_txt = "Nocturno 8-9 PM";
+            break;
+        case 7:
+            horario_txt = "Nocturno 10-11 PM";
+            break;
+        case 8:
+            horario_txt = "Nocturno 11-12 PM";
+            break;
+        default:
+            horario_txt = "Error";
+            break;
+    }
+
+    switch (gtk_combo_box_get_active(GTK_COMBO_BOX(data + 4))) {
+        case 0:
+            boleto_tipo = "Ordinario $50";
+            break;
+        case 1:
+            boleto_tipo = "1ra Directo $100";
+            break;
+        case 2:
+            boleto_tipo = "Gl Premier $150";
+            break;
+        default:
+            boleto_tipo = "Error";
+            break;
+    }
+
+    // Crear nueva ventana para mostrar los datos seleccionados
+    GtkWidget *window;
+    GtkWidget *label_nombre;
+    GtkWidget *label_destino;
+    GtkWidget *label_horario;
+    GtkWidget *label_boleto;
+    GtkWidget *grid;
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Datos Seleccionados");
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 150);
+
+    grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
+
+    label_nombre = gtk_label_new(NULL);
+    gtk_label_set_text(GTK_LABEL(label_nombre), nombre);
+    gtk_grid_attach(GTK_GRID(grid), label_nombre, 0, 0, 1, 1);
+
+    label_destino = gtk_label_new(NULL);
+    gtk_label_set_text(GTK_LABEL(label_destino), destino);
+    gtk_grid_attach(GTK_GRID(grid), label_destino, 0, 1, 1, 1);
+
+    label_horario = gtk_label_new(NULL);
+    gtk_label_set_text(GTK_LABEL(label_horario), horario_txt);
+    gtk_grid_attach(GTK_GRID(grid), label_horario, 0, 2, 1, 1);
+
+    label_boleto = gtk_label_new(NULL);
+    gtk_label_set_text(GTK_LABEL(label_boleto), boleto_tipo);
+    gtk_grid_attach(GTK_GRID(grid), label_boleto, 0, 3, 1, 1);
+
+    gtk_widget_show_all(window);
+}
+void on_button_show_data_clicked(GtkWidget *widget, gpointer data) { // boton de ver los registros
+
+    GtkWidget *parent_window = GTK_WIDGET(data);
+    GtkWidget *new_window;
+    GtkWidget *label;
+    gchar *nombre, *destino, *horario, *boleto;
+
+    // Obtener los datos de la ventana principal
+    GtkWidget *grid = gtk_bin_get_child(GTK_BIN(parent_window));
+    GtkWidget *entry = gtk_grid_get_child_at(GTK_GRID(grid), 1, 0);
+    GtkWidget *combo_destino = gtk_grid_get_child_at(GTK_GRID(grid), 1, 1);
+    GtkWidget *combo_horario = gtk_grid_get_child_at(GTK_GRID(grid), 1, 2);
+    GtkWidget *combo_boleto = gtk_grid_get_child_at(GTK_GRID(grid), 1, 3);
+
+    nombre = g_strdup_printf("%s", gtk_entry_get_text(GTK_ENTRY(entry)));
+    destino = g_strdup_printf("%s", gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo_destino)));
+    horario = g_strdup_printf("%s", gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo_horario)));
+    boleto = g_strdup_printf("%s", gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo_boleto)));
+    // Enviar la solicitud al 
+    // Copiar los valores desde los punteros a los miembros de la estructura
+    strcpy(nueva_solicitud.nombre, nombre);
+    strcpy(nueva_solicitud.destino, destino);
+    strcpy(nueva_solicitud.horario, horario);
+    strcpy(nueva_solicitud.boleto_tipo, boleto);
+
+    nueva_solicitud.tipo = 1; // Tipo de mensaje arbitrario
+    if (send(mi_socket, &nueva_solicitud, sizeof(struct solicitud), 0) == -1) {
+        perror("Error al enviar la solicitud al servidor");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Solicitud enviada al servidor.\n");
+    
+    // Recibir la confirmación del servidor
+    numbytes = recv(mi_socket, buffer, SIZE - 1, 0);
+    if (numbytes == -1) {
+        perror("Error al recibir la confirmación del servidor");
+        exit(EXIT_FAILURE);
+    }
+    buffer[numbytes] = '\0';
+    printf("%d bytes recibidos\n", numbytes);
+    printf("Recibido: %s\n", buffer);
+    
+    // Cerrar el socket
+    close(mi_socket);
+
+    // Crear nueva ventana para mostrar los datos
+    new_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(new_window), "Datos Seleccionados");
+    gtk_window_set_default_size(GTK_WINDOW(new_window), 200, 150);
+
+    grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(new_window), grid);
+
+    label = gtk_label_new(nombre);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
+    g_free(nombre);
+
+    label = gtk_label_new(destino);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
+    g_free(destino);
+
+    label = gtk_label_new(horario);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 2, 1, 1);
+    g_free(horario);
+
+    label = gtk_label_new(boleto);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 3, 1, 1);
+    g_free(boleto);
+
+    gtk_widget_show_all(new_window);
+}
+
+
+int main(int argc, char *argv[]) {
+    GtkWidget *window;
+    GtkWidget *button;
+    GtkWidget *grid;
+    GtkWidget *entry_nombre;
+    GtkWidget *combo_destino;
+    GtkWidget *combo_horario;
+    GtkWidget *combo_boleto;
+    GtkWidget *spin_horario;
+    GtkWidget *label_nombre;
+    GtkWidget *label_destino;
+    GtkWidget *label_horario;
+    GtkWidget *label_boleto;
+    
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    // SERVIDOR 
+    if (argc != 3) {
+        printf("error: modo de empleo: cliente ip puerto\n");
+        exit(-1);
+    }
+    
+    // Configurar la dirección del servidor
+    mi_estructura.sin_family = AF_INET;
+    mi_estructura.sin_port = htons(atoi(argv[2]));
+    inet_aton(argv[1], &(mi_estructura.sin_addr));
+    memset(&(mi_estructura.sin_zero), '\0', 8);
+    
+    // Crear el socket TCP
+    mi_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (mi_socket == -1) {
+        perror("Error al crear el socket");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Conectar al servidor
+    tam = sizeof(struct sockaddr);
+    if (connect(mi_socket, (struct sockaddr *)&mi_estructura, tam) == -1) {
+        perror("Error al conectar al servidor");
+        exit(EXIT_FAILURE);
+    }
+    
+    
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    // INTERFAZ
+
+    grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
+
+    label_nombre = gtk_label_new("Nombre:");
+    gtk_grid_attach(GTK_GRID(grid), label_nombre, 0, 0, 1, 1);
+
+    entry_nombre = gtk_entry_new();
+    gtk_grid_attach(GTK_GRID(grid), entry_nombre, 1, 0, 1, 1);
+
+    label_destino = gtk_label_new("Destino:");
+    gtk_grid_attach(GTK_GRID(grid), label_destino, 0, 1, 1, 1);
+
+    combo_destino = gtk_combo_box_text_new();
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_destino), "Ciudad de México");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_destino), "Guadalajara");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_destino), "Monterrey");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_destino), "Puebla");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_destino), "Tijuana");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_destino), "Mérida");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_destino), "Querétaro");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_destino), "Cancún");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_destino), "Acapulco");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_destino), "Oaxaca");
+    gtk_combo_box_set_active(GTK_COMBO_BOX(combo_destino), 0);
+    gtk_grid_attach(GTK_GRID(grid), combo_destino, 1, 1, 1, 1);
+
+    label_horario = gtk_label_new("Horario:");
+    gtk_grid_attach(GTK_GRID(grid), label_horario, 0, 2, 1, 1);
+
+    combo_horario = gtk_combo_box_text_new();
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_horario), "Matutino 7-8 AM");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_horario), "Matutino 9-10 AM");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_horario), "Matutino 11-12 AM");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_horario), "Vespertino 1-3 PM");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_horario), "Vespertino 3-5 PM");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_horario), "Vespertino 5-7 PM");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_horario), "Nocturno 8-9 PM");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_horario), "Nocturno 10-11 PM");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_horario), "Nocturno 11-12 PM");
+    gtk_combo_box_set_active(GTK_COMBO_BOX(combo_horario), 0);
+    gtk_grid_attach(GTK_GRID(grid), combo_horario, 1, 2, 1, 1);
+
+    label_boleto = gtk_label_new("Tipo de boleto:");
+    gtk_grid_attach(GTK_GRID(grid), label_boleto, 0, 3, 1, 1);
+
+    combo_boleto = gtk_combo_box_text_new();
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_boleto), "Ordinario $50");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_boleto), "1ra Directo $100");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_boleto), "Gl Premier $150");
+    gtk_combo_box_set_active(GTK_COMBO_BOX(combo_boleto), 0);
+    gtk_grid_attach(GTK_GRID(grid), combo_boleto, 1, 3, 1, 1);
+   
+
+    button = gtk_button_new_with_label("Mostrar Datos");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_button_show_data_clicked), (gpointer)window);
+    gtk_grid_attach(GTK_GRID(grid), button, 0, 6, 2, 1);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
+
+    return 0;
+}
+
+
---
+++ servidor.c
@@ -0,0 +1,90 @@
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define COLA_MENSAJES 1234
+
+struct solicitud {
+    long tipo;
+    char nombre[50];
+    char destino[50];
+    char horario[50];
+    char boleto_tipo[50];
+};
+
+int main(int argc, char *argv[]) {
+    int mi_socket, nuevo, tam;
+    struct sockaddr_in mi_estructura;
+    struct solicitud solicitud_recibida;
+    mi_estructura.sin_family = AF_INET;
+    mi_estructura.sin_port = 0; // Puerto 0 para que el sistema elija un puerto disponible
+    mi_estructura.sin_addr.s_addr = INADDR_ANY;
+    memset(&(mi_estructura.sin_zero), '\0', 8);
+    
+    // Crear el socket
+    mi_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (mi_socket == -1) {
+        perror("Error al crear el socket");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Enlazar el socket a una dirección y puerto específicos
+    tam = sizeof(struct sockaddr);
+    if (bind(mi_socket, (struct sockaddr *)&mi_estructura, tam) == -1) {
+        perror("Error al enlazar el socket");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Obtener la dirección y el puerto del socket después de enlazarlo
+    getsockname(mi_socket, (struct sockaddr *)&mi_estructura, &tam);
+    printf("El servidor está escuchando en el puerto: %d\n", ntohs(mi_estructura.sin_port));
+    
+    // Configurar el socket para escuchar conexiones entrantes
+    if (listen(mi_socket, 5) == -1) {
+        perror("Error al escuchar conexiones");
+        exit(EXIT_FAILURE);
+    }
+    
+    while (1) { // Bucle infinito para tratar conexiones
+        nuevo = accept(mi_socket, (struct sockaddr *)&mi_estructura, &tam);
+        if (nuevo == -1) {
+            perror("Error al aceptar la conexión entrante");
+            exit(EXIT_FAILURE);
+        }
+        
+        // Recibir la solicitud del cliente
+        if (recv(nuevo, &solicitud_recibida, sizeof(struct solicitud), 0) == -1) {
+            perror("Error al recibir la solicitud del cliente");
+            close(nuevo);
+            continue;
+        }
+        
+        // Registrar la solicitud recibida
+        printf("Solicitud recibida:\n");
+        printf("Nombre : %s\n", solicitud_recibida.nombre);
+        printf("Destino: %s\n", solicitud_recibida.destino);
+        printf("Horario: %s\n", solicitud_recibida.horario);
+        printf("Tipo de boleto: %s\n", solicitud_recibida.boleto_tipo);
+        
+        // Enviar confirmación al cliente
+        if (send(nuevo, "Solicitud recibida por el servidor\n", 35, 0) == -1) {
+            perror("Error al enviar la confirmación al cliente");
+            close(nuevo);
+            continue;
+        }
+        
+        // Cerrar la conexión con el cliente
+        close(nuevo);
+    }
+
+    // Cerrar el socket principal (esto nunca se alcanza debido al bucle infinito)
+    close(mi_socket);
+
+    return 0;
+}
+
